d98323a21eed8139df64d959c7becea098db6f13
aaron <aaroncabrale@uninorte.edu.co>
2023-06-25 15:49:35 -0500

    change of UI applied and working

--- catalog/views.py
+++ catalog/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     
-    return render(request, 'indexb.html')
+    return render(request, 'index.html')
 
 def info(request):
     return render(request, 'info.html')
+
+def nosotros(request):
+    return render(request, 'nosotros.html')
