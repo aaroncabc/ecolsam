bd93915d54256ee4b96a3ac3df571a60d36f6c1c
aaron <aaroncabrale@uninorte.edu.co>
2023-06-25 15:59:05 -0500

    links updated and button fixed

--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    
+def index(request):    
     return render(request, 'index.html')
-
-def info(request):
-    return render(request, 'info.html')
 
 def nosotros(request):
     return render(request, 'nosotros.html')
