57ceb373783596d02a7868a84167a0a2a02382e0
aaron <aaroncabrale@uninorte.edu.co>
2023-06-29 10:40:58 -0500

    blog first implementation

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.urls import reverse_lazy
+from django.utils import timezone
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    thumbnail = models.ImageField(upload_to='media/thumbnails')
+    description = models.TextField()
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+    def get_thumbnail_url(self):
+        return self.thumbnail.url
+
+    def get_absolute_url(self):
+        return reverse_lazy('post_detail', args=[str(self.pk)])
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def index(request):    
@@ -9,3 +10,10 @@ def nosotros(request):
 
 def info(request):
     return render(request, 'info.html')
+
+def blog(request):
+    posts = Post.objects.all()
+    context = {
+        'Posts':posts,
+    }
+    return render(request, 'blog.html', context=context)
